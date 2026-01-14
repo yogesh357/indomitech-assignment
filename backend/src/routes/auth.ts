c93c070420000ed5e5331fe3314bd64603f07ff3
yogesh357 <ypdhakane101@gmail.com>
2026-01-14 16:17:55 +0530

    fix

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,9 +1,6 @@
 import express from 'express';
 import { getMe, login, logout, register } from '../controllers/authController.js';
-import { protect } from '../middleware/auth.js';
-// import { register, login, getMe, logout } from '../controllers/authController.ts';
-// import { protect } from '../middleware/auth.ts';
-
+import { protect } from '../middleware/auth.js'; 
 const router = express.Router();
 
 router.post('/register', register);
